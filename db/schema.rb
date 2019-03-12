52b84a1d0e7cfc5feabca0e9b64f7eeca4c546f3
miller9 <millerpoli9@gmail.com>
2019-03-12 15:19:49 -0500

    Changing DB to use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_17_212439) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pics", force: :cascade do |t|
     t.string "title"
     t.text "description"
